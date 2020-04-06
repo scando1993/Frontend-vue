10188d2234867118612c77ecb9ed9986bb13e936
Allan <allan_ov1995@hotmail.com>
2020-04-06 10:09:16 -0500

    adding task store

--- src/store/index.js
+++ src/store/index.js
@@ -14,6 +14,7 @@ import planiVerticalSidebar from './modules/planiVerticalSidebar';
 import calendarNavbar from './modules/calendarNavBar';
 import clientData from './modules/clientData';
 import vendorData from './modules/VendorData';
+import taskData from './modules/taskData';
 
 
 // Load Vuex
@@ -35,6 +36,7 @@ export default new Vuex.Store({
     calendarNavbar,
     taskGridNavbar,
     clientData,
-    vendorData
+    vendorData,
+    taskData
   }
 });
---
+++ src/store/modules/taskData.js
@@ -0,0 +1,47 @@
+const axios = require('axios');
+export default {
+    state: {
+        tasks_list: []
+    },
+    getters: {
+        TASKS_LIST: state => state.tasks_list
+    },
+    mutations: {
+        SET_TASK_LIST(state, payload) {
+            state.tasks_list = payload;
+        }
+    },
+    actions: {
+        GET_TASKS_LIST: async ({commit}) => {
+            const config = {
+                headers: {'x-authorization': 'Bearer ' + localStorage.getItem('token')}
+            };
+            const response = await axios.get(process.env.VUE_APP_API + '/Task/getAll?limit=1000&textSearch=', config);
+            if (!response.data.error) {
+                console.log(response.data.data);
+                commit('SET_TASK_LIST', response.data.data.data)
+            } else {
+                console.log(response);
+            }
+        },
+        POST_TASK: ({commit}, data) => {
+            return new Promise((resolve, reject) => {
+                const config = {
+                    headers: {'x-authorization': 'Bearer ' + localStorage.getItem('token'),
+                        'Content-Type': 'application/json'},
+                };
+                axios
+                    .post(process.env.VUE_APP_API + '/Task/create', data, config)
+                    .then(({data, status}) => {
+                        console.log(data, status);
+                        // commit('ADD_CLIENT', data);
+                        resolve({data, status});
+                    })
+                    .catch( error => {
+                        reject(error);
+                    })
+            })
+        },
+
+    }
+}
