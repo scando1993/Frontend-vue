b3cc9fbb31448842a1f86693ad48135b1b323105
Sucre Cando <scando@fiec.espol.edu.ec>
2020-04-28 19:39:42 -0500

    Changes and merge with dev

--- src/views/app/report/report.vue
+++ src/views/app/report/report.vue
@@ -7,21 +7,24 @@
           <h3 class="text-center">Cumplimiento de tareas</h3>
           <p>Dentro del control de linea roja durante x cantidad de tiempo</p>
           <div class="chart-wrapper">
-            <apexchart type="donut" :options="calendarPriorityOptions" :series="calendarPriorityOptions.series" :autoresize="true"/>
+            <apexchart type="donut" :options="calendarPriorityOptions" :series="calendarPriorityOptions.series"
+                       :autoresize="true"/>
           </div>
         </b-col>
         <b-col md="12" sm="12" lg="4">
           <h3 class="text-center">Interacción con el cliente</h3>
           <p>Cantidades de veces que se interactuo con el cliente durante x cantidad de tiempo</p>
-          <div class="chart-wrapper" >
-            <apexchart type="donut" :options="clientPriorityOptions" :series="clientPriorityOptions.series" :autoresize="true"/>
+          <div class="chart-wrapper">
+            <apexchart type="donut" :options="clientPriorityOptions" :series="clientPriorityOptions.series"
+                       :autoresize="true"/>
           </div>
         </b-col>
         <b-col md="12" sm="12" lg="4">
           <h3 class="text-center">Tipos de tarea</h3>
           <p>Dentro de las horas de actividad durante x cantidad de tiempo</p>
           <div class="chart-wrapper">
-            <apexchart type="donut" :options="tasksPriorityOptions" :series="tasksPriorityOptions.series" :autoresize="true"/>
+            <apexchart type="donut" :options="tasksPriorityOptions" :series="tasksPriorityOptions.series"
+                       :autoresize="true"/>
           </div>
         </b-col>
       </b-row>
@@ -30,7 +33,8 @@
           <h3 class="text-center">Estatus del Cliente</h3>
           <p class="mx-auto">Total de clientes activos o inactivos durante x cantidad de tiempo</p>
           <div class="chart-wrapper mx-auto" style="width: 50%">
-            <apexchart type="bar" :options="clientsColumnOptions" :series="clientsColumnOptions.series" :autoresize="true"/>
+            <apexchart type="bar" :options="clientsColumnOptions" :series="clientsColumnOptions.series"
+                       :autoresize="true"/>
           </div>
         </b-col>
       </b-row>
@@ -45,7 +49,7 @@ import ReportsNavBar from './navbar/reportsNavBar';
 import { mapActions, mapGetters } from 'vuex';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
-import domtoimage from "dom-to-image";
+import domtoimage from 'dom-to-image';
 
 export default {
   name: 'report',
@@ -76,7 +80,7 @@ export default {
       'inactive': 20
     }
   }),
-  computed:{
+  computed: {
     // tasks: {
     //   get(){
     //     return this.TASKS_PROGRESS;
@@ -89,26 +93,26 @@ export default {
     //   },
     //   set(value){}
     // },
-    tasksPriorityOptions(){
+    tasksPriorityOptions() {
       return reportSetup.tasksPriorityOptions(this.TASKS_TYPE_REPORT);
     },
-    clientPriorityOptions(){
+    clientPriorityOptions() {
       return reportSetup.clientPriorityOptions(this.CLIENT_REPORT);
     },
-    calendarPriorityOptions(){
+    calendarPriorityOptions() {
       return reportSetup.calendarPriorityOptions(this.TASKS_PRIORITY_REPORT);
     },
-    clientsColumnOptions(){
+    clientsColumnOptions() {
       return reportSetup.clientsOptions(this.CLIENT_COLUMN_REPORT);
     },
-    clientsTimeseriesOptions(){
+    clientsTimeseriesOptions() {
 
     },
     ...mapGetters([
       'getSelectedFilter',
-            'TASKS_PRIORITY_REPORT',
-            'TASKS_TYPE_REPORT',
-            'CLIENT_REPORT',
+      'TASKS_PRIORITY_REPORT',
+      'TASKS_TYPE_REPORT',
+      'CLIENT_REPORT',
       'CLIENT_COLUMN_REPORT'
 
     ])
@@ -127,7 +131,7 @@ export default {
         }, 0);
       } else if ( mutation.type === 'setSendEmail' ) {
         console.log('email clicked');
-        setTimeout( () => {
+        setTimeout(() => {
           this.sendEmail();
         }, 0);
       }
@@ -146,22 +150,22 @@ export default {
       'TASKS_PROGRESS',
       'CLIENT_PROGRESS'
     ]),
-    generatePDF: function(){
+    generatePDF: function () {
       let currentDate = this.$moment((new Date()).getTime()).format('DD-MMMM');
       /** WITH CSS */
       domtoimage.toPng(this.$refs.content)
-        .then(function(dataUrl) {
+        .then(function (dataUrl) {
           let img = new Image();
           img.src = dataUrl;
           const doc = new jsPDF({
             orientation: 'portrait',
-            format: [900, 1400]
+            format: [ 900, 1400 ]
           });
           doc.addImage(img, 'JPEG', 20, 20);
           const filename = currentDate + '.pdf';
           doc.save(filename);
         })
-        .catch(function(error) {
+        .catch(function (error) {
           console.error('oops, something went wrong!', error);
         });
       // console.log(currentDate);
@@ -171,7 +175,8 @@ export default {
       //   doc.save(currentDate + '.pdf');
       // });
     },
-    sendEmail: function(){}
+    sendEmail: function () {
+    }
   }
 };
 </script>
